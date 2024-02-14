1ade9decf4e655d548a6be885940ee997ce11dc9
Raja Sekhar Kollipara <rajasekharkollipara17@gmail.com>
2024-02-14 18:54:53 +0530

    Add project files.

---
+++ Controllers/StudentMgmtController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StudentMgmtSystem.Controllers
+{
+    public class StudentMgmtController : Controller
+    {
+        // GET: StudentMgmt
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
